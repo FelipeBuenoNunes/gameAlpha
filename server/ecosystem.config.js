4458c8d9933d10a17d519a51b3bbfbfaa6d5a99e
Augusto Braga <bragaaugusto361@gmail.com>
2022-03-24 13:48:29 -0300

    correcao ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    apps: [
-      {
-        script: 'server.js',
-        cwd: 'front/',
-        name: 'Frontend',
-        watch: true
-      },
-      {
-        script: 'app.js',
-        cwd: 'back/',
-        name: 'Backend',
-        watch: true
-      }
-    ]
-  }
+  apps: [
+    {
+      script: "server.js",
+      cwd: "frontend/",
+      name: "Frontend",
+      watch: true,
+    },
+    {
+      script: "app.js",
+      cwd: "backend/",
+      name: "Backend",
+      watch: true,
+    },
+  ],
+};
